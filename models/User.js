e8801882938a4f09976b7fbb48b5e00545ed7f93
= <vasiu.catalina10@gmail.com>
2025-03-19 14:01:52 +0200

    add user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        firstname: { type: String, required: true },
+        lastname: { type: String, required: true },
+        birthDate: { type: Date, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        phone: { type: String, required: true },
+        role: { type: String, enum: ["admin", "trainer", "client"] },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("User", userSchema);
