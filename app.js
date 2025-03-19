4aeb65ab03bdb13c037c800edfb036a334cb1d28
= <vasiu.catalina10@gmail.com>
2025-03-19 13:49:37 +0200

    add db connections

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const db = require("./db");
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("App listening on port 3000");
+db.connect().then(()=> {
+    app.listen(3000, ()=>{
+        console.log("App listening on port 3000");
+    });
 });
---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connect = async () => {
+    const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+    console.log(DB_USERNAME, DB_PASSWORD, DB_NAME);
+    const uri = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@127.0.0.1:27017/${DB_NAME}?authSource=admin`;
+    console.log(uri);
+    try {
+        await mongoose.connect(uri);
+        console.log("Connected to database");
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+module.exports = {connect}
