ee33ecd847dc0d2245b3c50be21caae012c32f12
= <vasiu.catalina10@gmail.com>
2025-04-01 10:57:51 +0300

    simplify models

--- models/CheckInLog.js
+++ models/CheckInLog.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const checkInLogSchema = new mongoose.Schema(
     {
         user:{type:mongoose.Schema.Types.ObjectId, ref:"User"},
-        fitnessCenter:{type:mongoose.Schema.Types.ObjectId, ref:"FitnessCenter"},
-        subscription:{type:mongoose.Schema.Types.ObjectId, ref:"Subscription"},
         duration: { type: Number, required: true, min:0 },
         checkInTime: { type: Date, required: true },
         checkOutTime: { type: Date, required: true },
